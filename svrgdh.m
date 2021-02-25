7684c98b19a41859740266aed927a3b8a4007107
hardy-opt <76877502+hardy-opt@users.noreply.github.com>
2021-02-25 19:03:29 +0900

    Update svrgdh.m

--- svrgdh.m
+++ svrgdh.m
@@ -68,11 +68,11 @@ function [w, infos] = svrgdh(problem, in_options)
     while  (epoch < options.max_epoch)
 
         % permute samples
-        if options.permute_on
-            perm_idx = randperm(n);
-        else
+       % if options.permute_on
+       %     perm_idx = randperm(n);
+       % else
             perm_idx = 1:n;
-        end
+       % end
 
         % compute full gradient
         full_grad = problem.full_grad(w);
